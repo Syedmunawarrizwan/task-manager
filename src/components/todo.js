1fab0f678a85ff4f13b8a84a2d0866828a6382bd
munawar <syedmunawarrizwan17@gmail.com>
2022-05-21 20:07:12 +0530

    icons added

--- src/components/todo.js
+++ src/components/todo.js
@@ -114,7 +114,9 @@ function Todo() {
             {!(showInputId == item.id) && <h1 className="todo-list">{item.entry}</h1>}
             {(showInputId == item.id) && <div>
               <input value={editData} onChange={(e) => { setEditData(e.target.value) }}></input>
-              <button onClick={saveChanges.bind(null, item.id)}>Save</button>
+              <button onClick={saveChanges.bind(null, item.id)}>
+                <img src="https://cdn-icons.flaticon.com/png/512/4436/premium/4436481.png?token=exp=1653144176~hmac=b1d82ec1e28a0944ea16839a1db75c4d"></img>
+              </button>
             </div>}
 
             <button className="delete-btn"
@@ -122,9 +124,9 @@ function Todo() {
                 DeleteTodo(item.id);
               }}
             >
-              <img src="https://cdn-icons-png.flaticon.com/512/3096/3096687.png" />
+              <img src="https://cdn-icons-png.flaticon.com/512/1828/1828843.png" />
             </button>
-            <button onClick={() => { UpdateTodo(item) }}>Update</button>
+            <button onClick={() => { UpdateTodo(item) }}><img src="https://cdn-icons.flaticon.com/png/512/420/premium/420140.png?token=exp=1653144448~hmac=5970243b17bbc9d0f093b71869b41e94"></img></button>
 
           </div>
         );
